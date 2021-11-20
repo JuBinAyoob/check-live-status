c04424302185848e8e862ff0a883ae85cf42a211
Jubin Ayoob <jubinayoob369@gmail.com>
2021-11-20 10:22:27 +0530

    Add files via upload

---
+++ config/config.js
@@ -0,0 +1,15 @@
+const mongodb = "mongodb://jub:live12345@ds161092.mlab.com:61092/livedomain";
+
+const waitingTimeAfterAllUrlCheck = 1000;
+
+const waitingTimeForCheckingOneUrl = 1000;
+
+const MAILER_SERVER_TOKEN = "65909abf-17df-4f2a-ae28-eec17437aaf2";
+const MAILER_SENDER_EMAIL = "nverdhan@codalyze.com";
+const MAILER_RECEIVER_EMAIL = "jubin@codalyze.com";
+
+
+
+module.exports = {
+    mongodb, waitingTimeAfterAllUrlCheck, waitingTimeForCheckingOneUrl, MAILER_SERVER_TOKEN, MAILER_SENDER_EMAIL, MAILER_RECEIVER_EMAIL
+};
